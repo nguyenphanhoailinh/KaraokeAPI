64845b6eede57d821eaddce115d2ffd4b5b0d8c7
nguyenphanhoailinh <nguyenphanhoailinh27@gmail.com>
2023-06-09 23:40:50 +0700

    update

--- Data/MyDbContext.cs
+++ Data/MyDbContext.cs
@@ -12,6 +12,8 @@ namespace testAPI.Data
        
         #region DbSet
         public DbSet<KaraokeRoom> karaokeRoom { get; set; }
+
+        public DbSet<Customer> customer { get; set; }
         #endregion
     }
 }
